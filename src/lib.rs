32029640a33b8ff87b0ea128d187d8a3edb61695
anak10thn <anak10thn@gmail.com>
2019-10-03 13:20:49 +0700

    ADD: native db connection #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,46 @@
 extern crate sqlite;
+extern crate libc;
 //use sqlite::{Connection, OpenFlags, State, Type, Value};
+use libc::{c_char};
+use std::ffi::CStr;
+
 #[no_mangle]
-pub extern fn echo(echo: &str) -> &str {
-    echo
+pub extern fn koneksi(koneksi: &str) -> &str{
+    koneksi
 }
 
 #[no_mangle]
-pub extern fn bsqlite(){
-    let connection = sqlite::open(":memory:").unwrap();
-    connection
+pub extern fn execute(koneksi: *const c_char, query: *const c_char) {
+    let koneksi = unsafe {
+        assert!(!koneksi.is_null());
+        CStr::from_ptr(koneksi)
+    };
+    let query = unsafe {
+        assert!(!query.is_null());
+        CStr::from_ptr(query)
+    };
+
+    let connection = sqlite::open(koneksi.to_str().unwrap()).unwrap();
+
+    let ddl = ["create","insert", "drop"];
+    let query_first: Vec<&str> = query.to_str().unwrap().split(" ").collect();
+    if ddl.contains(&query_first[0].to_lowercase().as_str()){
+        connection
         .execute(
-            "
-            CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER);
-            INSERT INTO users VALUES ('Alice', 42);
-            INSERT INTO users VALUES ('Bob', 69);
-            ",
+            query.to_str().unwrap(),
         )
         .unwrap();
-    connection
-        .iterate("SELECT * FROM users WHERE age > 50", |pairs| {
+    }
+    else{
+        connection
+        .iterate(query.to_str().unwrap(), |pairs| {
             for &(column, value) in pairs.iter() {
                 println!("{} = {}", column, value.unwrap());
             }
             true
         })
         .unwrap();
+    }
 }
 
 #[allow(dead_code)]
